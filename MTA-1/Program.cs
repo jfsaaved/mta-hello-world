edae896e7a93193d420a86e9b21c9ab6b167dfea
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2019-08-06 11:57:27 -0400

    init

---
+++ MTA-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MTA_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
